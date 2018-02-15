483d6ee1e49f101c215323585d54d71faef03b05
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-02-15 14:09:39 +0100

    Allow Content-Type Header

--- handler/handler.go
+++ handler/handler.go
@@ -10,6 +10,7 @@ import (
 func respond(w http.ResponseWriter, body []byte, code int) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
